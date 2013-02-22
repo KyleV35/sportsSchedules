9f244767fbf008dc34b1bdd3f4c1a1d95dd4e48a
Kyle Vermeer <kvermeer@stanford.edu>
2013-02-22 23:48:40 +0000

    Changed url scheme

--- controllers/api.js
+++ controllers/api.js
@@ -6,12 +6,17 @@ var connectionString = "pg://" + database.user + ":" + database.password + "@" +
 var team = require('../models/team');
 
 exports.findAllNBATeams = function(req,res) {
+	// Initialize client and response
 	var client = new pg.Client(connectionString);
+	client.connect();
 	var JSON_response = {};
 	var teams_array = [];
 	JSON_response.teams = teams_array;
-	client.connect();
+
+	// Find all NBA Teams
     var query = client.query('SELECT * from teams NATURAL JOIN NBA_teams');
+
+    // For each team, create team and push JSON to teams_array
   	query.on('row', function(row) {
   		var NBA_team = new team.NBATeam(row["team_id"],row["name"],row["city"],row["state"],row["stadium"],row["conference"]);
   		teams_array.push(NBA_team.toJSON());
@@ -33,10 +38,10 @@ exports.findAllNBATeams = function(req,res) {
 
 exports.findNBATeamByID = function(req,res) {
 	var client = new pg.Client(connectionString);
+	client.connect();
 	var id = req.params.id;
 	var JSON_response = {};
 	JSON_response.team = "";
-	client.connect();
     var query = client.query('SELECT * from teams NATURAL JOIN NBA_teams where team_id='+id);
     query.on('row', function(row) {
   		var NBA_team = new team.NBATeam(row["team_id"],row["name"],row["city"],row["state"],row["stadium"],row["conference"]);
--- routes.js
+++ routes.js
@@ -4,8 +4,8 @@ exports.setUpRoutes = function(app) {
 	app.get("/", function(req,res) {
 		res.send("I'm here!");
 	});
-	app.get("/nba-teams", api_controller.findAllNBATeams);
-	app.get("/nba-teams/:id", api_controller.findNBATeamByID);
+	app.get("teams/nba", api_controller.findAllNBATeams);
+	app.get("teams/nba/:id", api_controller.findNBATeamByID);
 	app.get("/test", function(req,res) {
 		res.send("Hello");
 	});
