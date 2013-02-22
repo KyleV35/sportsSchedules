4efcd86fcaf34a900bd2fc47666c8b5f960dc30b
Kyle Vermeer <kvermeer@stanford.edu>
2013-02-22 23:49:51 +0000

    Need / at start of url path

--- routes.js
+++ routes.js
@@ -4,8 +4,8 @@ exports.setUpRoutes = function(app) {
 	app.get("/", function(req,res) {
 		res.send("I'm here!");
 	});
-	app.get("teams/nba", api_controller.findAllNBATeams);
-	app.get("teams/nba/:id", api_controller.findNBATeamByID);
+	app.get("/teams/nba", api_controller.findAllNBATeams);
+	app.get("/teams/nba/:id", api_controller.findNBATeamByID);
 	app.get("/test", function(req,res) {
 		res.send("Hello");
 	});
