4eedfed5d71746f96c03914089c3f92c066d230c
Kyle Vermeer <kvermeer@stanford.edu>
2013-02-22 22:09:31 +0000

    Added JSON response

--- controllers/api.js
+++ controllers/api.js
@@ -7,16 +7,25 @@ exports.mainTest = function(req,res) {
 	var client = new pg.Client(connectionString);
 	console.log("Hit again!");
 	console.log("Client: "+ client);
+	var JSON_response = {};
+	var teams_array = [];
+	JSON_response.teams = teams_array;
 	client.connect();
     var query = client.query('SELECT * from teams');
     console.log("Query: " +query);
     var response = "Kyle";
   	query.on('row', function(row) {
-  		response += "<p>" + row["name"] + "</p>";
+  		var team_dict = {
+  			"name": row["name"],
+  			"city": row["city"],
+  			"state": row["state"],
+  			"stadium": row["stadium"]
+  		};
+  		teams_array.push(team_dict);
   	});
   	query.on('end', function(result) {
   		console.log("done");
   		client.end();
-  		res.send(response);
+  		res.json(JSON_response);
   	});
 }
